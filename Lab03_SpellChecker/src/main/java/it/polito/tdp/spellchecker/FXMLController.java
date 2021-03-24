30dc9123942aaeabe95f5c4fb7a2cedff002c22a
giuli <giuli@LAPTOP-RDOB3A6B>
2021-03-24 11:33:00 +0100

    Esercizio sul dizionario con errori lanciati a fine metodo.

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,84 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
+	Dictionary englishDictionary=new Dictionary();
+	Dictionary italianDictionary=new Dictionary();
+	String dictionarySetted = "English";
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private TextArea areaDaCoreggere;
+
+    @FXML
+    private TextArea wrongWordsList;
+
+    @FXML
+    private Label labelErrori;
+
+    @FXML
+    private Label labelTimeUsed;
+
+    @FXML
+    void check(ActionEvent event) {
+    	String toBeCorrected = areaDaCoreggere.getText().toLowerCase();
+    	if(dictionarySetted.equals("English")) {
+    		String corrected = englishDictionary.printableWrongWords(toBeCorrected);
+    		wrongWordsList.setText(corrected);
+    		labelErrori.setText(String.valueOf(englishDictionary.numberWrongWords(toBeCorrected)));
+    	}
+    	if(dictionarySetted.equals("Italian")) {
+    		String corrected = italianDictionary.printableWrongWords(toBeCorrected);
+    		wrongWordsList.setText(corrected);
+    		labelErrori.setText(String.valueOf(italianDictionary.numberWrongWords(toBeCorrected)));
+    	}
+    	
+    	
+
+    }
+
+    @FXML
+    void choosedEnglish(ActionEvent event) throws IOException {
+    	if(englishDictionary.getAllowedWords()==null) {
+    		englishDictionary.loadDictionary("English.txt");
+    	}
+    	dictionarySetted="English";
+    }
+
+    @FXML
+    void choosedItalian(ActionEvent event) throws IOException {
+    	if(italianDictionary.getAllowedWords()==null) {
+    		italianDictionary.loadDictionary("English.txt");
+    	}
+    	dictionarySetted="Italian";
+    }
+
+    @FXML
+    void clear(ActionEvent event) {
+    	labelErrori.setText("0");
+    	wrongWordsList.setText("");
+    	
+
+    }
+
+    @FXML
     void initialize() {
+        assert areaDaCoreggere != null : "fx:id=\"areaDaCoreggere\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert wrongWordsList != null : "fx:id=\"wrongWordsList\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert labelErrori != null : "fx:id=\"labelErrori\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert labelTimeUsed != null : "fx:id=\"labelTimeUsed\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
-
-
